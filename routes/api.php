e487f65a489dc0005b24f248a3b0980ec6cc1c2c
GlennWinter <winter.glenn@hotmail.co.uk>
2026-08-18 20:36:10 +0100

    fix: nest shopping list item routes under shopping lists

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,4 @@ use App\Http\Controllers\ShoppingListItemController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('shopping-lists', ShoppingListController::class);
-Route::apiResource('shopping-list-items', ShoppingListItemController::class);
+Route::apiResource('shopping-lists.items', ShoppingListItemController::class);
